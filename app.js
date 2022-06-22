78dedee74857370ddf9bb857fe9752f2d9067ec7
Nitin Sharma <nitin10.sharma@ril.com>
2022-06-22 22:33:24 +0530

    configuration

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log('Server started on port '+ port);
 });
